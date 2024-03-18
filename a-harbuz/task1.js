d77c2609b37c7fef2a3fa24bcaef3dc073d1903b
pamail08@gmail.com <pamail08@gmail.com>
2024-03-18 13:53:07 +0100

    task1.0.1

--- a-harbuz/task1.js
+++ a-harbuz/task1.js
@@ -1,6 +1,7 @@
 db.users.updateMany(
-    {country: {$nin['Germany','France']},
-        $gt: {balance: 0}
+    {
+        country: {$nin:['Germany','France']},
+        balance: {$gt: 0}
     },
-    {$mul: {balance: 1.03}}
+    {$mul: {balance: 0.97}}
 )
